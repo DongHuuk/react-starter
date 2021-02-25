f7aa9ca05affd6079556c56e056e6d0c4de18a0d
dong <drivespublic@gmail.com>
2021-02-25 13:49:06 +0900

    React Project Init

---
+++ src/Say.js
@@ -0,0 +1,23 @@
+import React, { useState } from 'react';
+
+const Say = () => {
+    const [messgae, setMessage] = useState("초기값");
+    const onClickEnter = () => setMessage("안녕하세요")
+    const onClickLeave = () => setMessage("안녕히가세요")
+
+
+    return (
+        <div>
+            <button onClick={onClickEnter}>
+                입장
+            </button>
+            <button onClick={onClickLeave}>
+                퇴장
+            </button>
+            <h1>{messgae}</h1>
+
+        </div>
+    )
+}
+
+export default Say;
